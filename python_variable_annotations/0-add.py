d1a4a2aec56755393b433d33ae90615665785493
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-09-27 23:04:48 +0000

    function concat srt

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""function return sum of float"""
+
 
 def add(a: float, b: float) -> float:
+    """return suma"""
     return a + b
---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""function of concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concatenate two strings"""
+    return str1 + str2
