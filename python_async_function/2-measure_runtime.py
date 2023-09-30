95304e6f5efbe5d2383df453c9f854fbbb0c5d82
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-09-30 04:44:22 +0000

    correct ouput

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -9,7 +9,7 @@ from concurrent_coroutines import wait_n
 def measure_time(n: int, max_delay: int) -> float:
     """return total of time """
     start_time = time.time()
-    wait_n(n, max_delay)
+    await wait_n(n, max_delay)
     end_time = time.time()
     total_time = end_time - start_time
     return total_time / n
