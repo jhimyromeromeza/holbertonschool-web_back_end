34960c058bc24420700740ba29a8f0182a8afe0a
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-09-30 23:32:17 +0000

    3-tasks.py

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""fnction that return asyncio"""
+
+
+import asyncio
+from basic_async_syntax import wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.task:
+    """return asyncio task"""
+    return asyncio.create_task(wait_random(max_delay))
