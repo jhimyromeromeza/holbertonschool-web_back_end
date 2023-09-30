fb540f83a52cc68117a01015980fa1cb210a4f1a
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-09-30 23:35:32 +0000

    correct the atribute

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -6,6 +6,6 @@ import asyncio
 from basic_async_syntax import wait_random
 
 
-def task_wait_random(max_delay: int) -> asyncio.task:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """return asyncio task"""
     return asyncio.create_task(wait_random(max_delay))
