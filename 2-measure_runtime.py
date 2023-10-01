9ec9ac79716a07762f6e2ed2922fb53f3086e7f0
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-10-01 04:38:34 +0000

    function that storage time

---
+++ 2-measure_runtime.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""function async"""
+
+
+import asyncio
+from async_comprehension import async_comprehension
+import time
+
+
+async def measure_runtime():
+    start_time = time.time()
+    tasks = [async_comprehension() for _ in range (4)]
+    await asyncio.gather(*tasks)
+    end_time = time.time()
+    return end_time - start_time
