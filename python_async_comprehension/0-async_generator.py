2304711ac69d0fba4469ba2046b192fa015f1085
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-10-01 01:14:07 +0000

    await ouput Generator float

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,9 +4,10 @@
 
 import asyncio
 import random
+from typing import Generator
 
-
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
+    """return number generator"""
     for _ in range(0, 10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
