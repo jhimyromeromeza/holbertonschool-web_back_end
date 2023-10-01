eed79fc8f0654a10d05d15ca2d006a76ec23e9e2
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-10-01 01:15:51 +0000

    correct pycodestyle

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -6,6 +6,7 @@ import asyncio
 import random
 from typing import Generator
 
+
 async def async_generator() -> Generator[float, None, None]:
     """return number generator"""
     for _ in range(0, 10):
