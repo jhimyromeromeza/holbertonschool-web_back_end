c40bd19d8f6cd9ec430749bf8b1738f743f0f93f
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-10-01 01:53:04 +0000

    create function that storage date float

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""function"""
+
+
+import async_generator import async_generator
+import asyncio
+import List
+
+
+async def async_comprehension() -> List[float]:
+    """return list of date float generator for a function"""
+    return [i async for i in async_generator()]
