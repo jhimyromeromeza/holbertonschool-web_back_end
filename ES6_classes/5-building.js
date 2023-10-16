4e4291eb55591382810679f27b71412b8495d013
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-10-15 23:18:40 -0500

    create class abstract

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,14 @@
+class Building {
+    constructor(sqft) {
+        if (this.constructor === Building) {
+            throw new Error('No puedes instanciar la clase abstracta!');
+        }
+        if (this.evacuationWarningMessage === undefined) {
+            throw new TypeError('Class extending Building must override evacuationWarningMessage');
+        }
+        this._sqft = sqft;
+    }
+    get sqft() {
+        return this._sqft;
+    }
+}
