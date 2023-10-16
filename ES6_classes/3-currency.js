39609ccd126091002eb7eff58d64be864d17892a
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-10-15 21:16:58 -0500

    class print valur de name and code

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,33 @@
+export default class Currency {
+  constructor(code, name) {
+    if(typeof(code) !== 'string') {
+      throw typeError('Code must be a string');
+    }
+    if(typeof(name) !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    this._code = code;
+    this._name = name;
+  }
+  get code() {
+    return this._code;
+  }
+  set code(newcode) {
+    if (typeof(newcode) !== 'string') {
+      throw TypeError('Code must be a string');
+    }
+    this._code = newcode;
+  }
+  get name() {
+    return this._name;
+  }
+  set name(newname) {
+    if(typeof(newname) !== 'string'){
+      throw TypeError('Name must be a string');
+    }
+    this._name = newname;
+  }
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`
+  }
+}
