f64c9b3a89fcfd15b5010036127baafd3f28dff4
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-10-25 18:14:06 -0500

    created function that generator exceptiones

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,12 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+  let temp;
+  try {
+    temp = mathFunction(); 
+  } catch (error) {
+    temp = error.toString();
+  }
+  queue.push(temp);
+  queue.push('Guardrail was processed');
+  return queue;
+}
