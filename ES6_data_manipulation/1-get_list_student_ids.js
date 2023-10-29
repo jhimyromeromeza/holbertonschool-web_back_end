5dedec7592e62651b9f37aae90f1a17d075b6dbd
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-10-29 16:30:08 -0500

    eslint

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,5 +2,5 @@ export default function getListStudentIds(getListStudents) {
   if (!Array.isArray(getListStudents)) {
     return [];
   }
-  return getListStudents.map((student) => student.id)
+  return getListStudents.map((student) => student.id);
 }
