70d6d46a82073edbe8550627206c7cfd9b87dc4a
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-10-29 17:04:21 -0500

    create funtion use filter

---
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,6 @@
+export default function getStudentsByLocation(getListStudents, city) {
+  if (!Array.isArray(getListStudents)) {
+    return [];
+  }
+  return getListStudents.filter((students) => students.location === city);
+}
