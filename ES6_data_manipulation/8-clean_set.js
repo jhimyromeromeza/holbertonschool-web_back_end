fc456e16f59028036978d5d07b5a65c671c50d43
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-10-29 19:42:48 -0500

    use slice

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,14 @@
+export default function cleanSet(set, startString) {
+  if (startString === '') {
+    return '';
+  }
+
+  let result = '';
+  for (const item of set) {
+    if (item.startsWith(startString)) {
+      result += `${item.slice(startString.length)}-`;
+    }
+  }
+
+  return result.slice(0, -1); // Elimina el último guión
+}
